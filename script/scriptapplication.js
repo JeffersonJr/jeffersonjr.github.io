678fa21b801c3d155c6ae7e6dad54d172eb6926f
Jefferson Campos <contato@jeffersonjunior.com.br>
2021-07-04 16:00:48 -0300

    Revert "newsite"
    This reverts commit a319ca527c3d1f07e86417cf94655c6a17f9f6cf.

---
+++ script/scriptapplication.js
@@ -0,0 +1,11 @@
+window.NREUM || (NREUM = {});
+NREUM.info = {
+	"beacon": "bam.nr-data.net",
+	"errorBeacon": "bam.nr-data.net",
+	"licenseKey": "e3900c3a01",
+	"applicationID": "2183715",
+	"transactionName": "dwwMREANVV4GRRsXCEEEBVFQDlxBTERcCxM=",
+	"queueTime": 0,
+	"applicationTime": 286,
+	"agent": ""
+}
